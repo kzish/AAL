0ea8e0b408d920ece6eecd4bae1c8f7335c366b1
kzish <zishumbak@gmail.com>
2022-03-03 13:49:14 +0200

    wip

---
+++ SharedProject/ApiModels/apiTutor.cs
@@ -0,0 +1,51 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Globals
+{
+    //[ElasticsearchType]
+    [ElasticsearchType(RelationName = "apiTutor", IdProperty = "AspnetUserId")]
+    public class apiTutor
+    {
+        [Keyword]
+        public string AspnetUserId { get; set; }
+        [Keyword]
+        public string Firstname { get; set; }
+        [Keyword]
+        public string Surname { get; set; }
+        [Keyword]
+        public string Email { get; set; }
+        [Keyword]
+        public string ImageUrl { get; set; }
+        [Keyword]
+        public string CoutryIso { get; set; }
+        [Keyword]
+        public string CoutryName { get; set; }
+        [Text]
+        public string About { get; set; }
+        [Object]
+        public List<Language> Languages { get; set; } = new List<Language>();
+        [Text]
+        public List<string> Courses { get; set; } = new List<string>();
+        [Keyword]
+        public string Mobile { get; set; }
+        [Keyword]
+        public string OtherMobile { get; set; }
+        //[Boolean(NullValue = false, Store = true)]
+        public sbyte MobileAvailableOnWhatsapp { get; set; }
+        //[Boolean(NullValue = false, Store = true)]
+        public sbyte ShowEmail { get; set; }
+    }
+
+    public class Language
+    {
+        [Keyword]
+        public int level { get; set; }
+        [Keyword]
+        public string lang { get; set; }
+
+    }
+}
---
+++ SharedProject/Globals/EsRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedModels;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace Globals
+{
+    public class EsRepository
+    {
+
+        private apiTutor ConverAspnetUserObjectToEsModel(string aspnet_user_id)
+        {
+            var tutor = AppSettings.db.Aspnetusers
+                        .Where(i => i.Id == aspnet_user_id)
+                        .Include(i => i.MTutor)
+                        .Include(i => i.MAspnetUserAvailableTimes)
+                        .Include(i => i.MAspnetUserLanguages)
+                        .Include(i => i.MMoodleUsers)
+                        .Include(i => i.MTutorCourses)
+                        .Include(i => i.MTutorEducations)
+                        .Include(i => i.MTutorRatings)
+                        .Include(i => i.MTutorWorkExperiences)
+                        .FirstOrDefault();
+
+            var apiTutor = new apiTutor()
+            {
+                //_id = tutor.Id,
+                AspnetUserId = tutor.Id,
+                Firstname = tutor.MTutor.Firstname,
+                Surname = tutor.MTutor.Surname,
+                Email = tutor.MTutor.Email,
+                ImageUrl = tutor.MTutor.ImageUrl,
+                CoutryIso = tutor.MTutor.CoutryIso,
+                CoutryName = AppSettings.db.MCountries.Find(tutor.MTutor.CoutryIso).CountryName,
+                About=tutor.MTutor.About,
+                //Languages { get; set; } = new List<Language>();
+                //Courses { get; set; } = new List<string>();
+                Mobile = tutor.MTutor.Mobile,
+                OtherMobile = tutor.MTutor.OtherMobile,
+                MobileAvailableOnWhatsapp = tutor.MTutor.MobileAvailableOnWhatsapp,
+                ShowEmail = tutor.MTutor.ShowEmail,
+            };
+
+
+           return apiTutor;
+        }
+        public dynamic IndexTutor(string aspnet_user_id) 
+        {
+            try
+            {
+
+                var tutor = ConverAspnetUserObjectToEsModel(aspnet_user_id);
+                
+                var indexResponse = AppSettings.EsClient.Index(tutor, i=>i.Index("tutors"));
+
+                dynamic res = new ExpandoObject();
+                res.res = "ok";
+                res.msg = "document indexed";
+                return res;
+            }
+            catch (Exception ex)
+            {
+                dynamic err = new ExpandoObject();
+                err.res = "err";
+                err.msg = ex.Message;
+                return err;
+            }
+
+
+        }
+    }
+}
